33df4797e6fd7e6f971726eaa20049abc7410924
iuri lourenço trovão <114348640+Iuri-Trovao@users.noreply.github.com>
2023-05-10 15:22:05 -0300

    path

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('../db/db')
 
-app.get('/', (req,res)=>{
+app.get('./view/index.html', (req,res)=>{
     res.send("hello world")
 })
 
