5e8c8284f2393380a3f07915755a0c59614e36bc
Tauan18 <TauanNovotec18@gmail.com>
2023-06-23 15:42:33 -0300

    atualização darkmode
    isso ae

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,6 +1,14 @@
 const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
+ var DarkMode = document.body.classList.toggle('dark-mode')
+  localStorage.setItem("darkmode", DarkMode)
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  var DarkM = localStorage.getItem("darkmode")
+  if (DarkM === "true") {
+    document.body.classList.add("dark-mode")
+}
 })
   
