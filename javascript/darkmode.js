12596549b5859dc909c24ba195337ba393813235
Tauan18 <TauanNovotec18@gmail.com>
2023-06-26 16:36:00 -0300

    conserto darkmode no botão

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,14 +1,25 @@
 const chk = document.getElementById('chk')
+const check = document.getElementById('ball')
+const label = document.getElementById('label')
 
 chk.addEventListener('change', () => {
  var DarkMode = document.body.classList.toggle('dark-mode')
   localStorage.setItem("darkmode", DarkMode)
+  if(DarkMode === true) {
+    check.style.transform = "translateX(24px)"
+  } else {
+    check.style.transform = "translateX(0px)"
+  }
 })
 
 document.addEventListener("DOMContentLoaded", () => {
-  var DarkM = localStorage.getItem("darkmode")
-  if (DarkM === "true") {
+  var isDarkMode = localStorage.getItem("darkmode") === "true"
+  if(isDarkMode) {
     document.body.classList.add("dark-mode")
-}
+    check.style.transform = "translateX(24px)"
+  } else {
+    document.body.classList.remove("dark-mode")
+    check.style.transform = "translateX(0px)"
+  }
 })
-  
+
