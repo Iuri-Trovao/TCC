40496b11a682a68d5ca974088d64893765dfa4d5
mauriciorodrigues123 <125503314+mauriciorodrigues123@users.noreply.github.com>
2023-09-04 18:55:28 -0300

    t
    t

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+var botao1 = document.getElementById('botao1')
+var botao2 = document.getElementById('botao2')
+
+botao1.addEventListener('click', function(){
+    botao1.style.display = 'none'
+    botao2.style.display = 'block'
+
+})
+
+botao2.addEventListener('click', function(){
+    botao1.style.display = 'block'
+    botao2.style.display = 'none'
+})
