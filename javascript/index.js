997c4b4e9debbcf1a17c4ed29991515a37775f99
iuri lourenço trovão <114348640+Iuri-Trovao@users.noreply.github.com>
2023-09-04 17:53:07 -0300

    path

--- javascript/index.js
+++
@@ -1,5 +0,0 @@
-const  Button_Menu = document.getElementById('button-menu')
-const Menu_Mobile = document.getElementById('menu-mobile-none')
-Button_Menu.onclick = ()=>{
-    Menu_Mobile.classList.toggle('menu-mobile')
-}
