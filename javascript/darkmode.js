a8c9bef74f3eaafcd06bb9acfb028f9e49a87002
mauriciorodrigues123 <125503314+mauriciorodrigues123@users.noreply.github.com>
2023-11-10 16:35:07 -0300

    o

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,14 +1,11 @@
 const toggleCheckbox = document.getElementById("toggle_checkbox");
 const mainElement = document.querySelector("main");
-var paragrafo = document.getElementById("paragrafo")
 
 toggleCheckbox.addEventListener("change", () => {
   if (toggleCheckbox.checked) {
     // Dark mode ativado
     mainElement.style.backgroundColor = "#333";
     mainElement.style.color = "#fff";
-    paragrafo.style.color = '#3455'
-    
     localStorage.setItem("darkMode", "enabled");
   } else {
     // Dark mode desativado
